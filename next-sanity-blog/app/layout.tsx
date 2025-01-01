f32e6a87275d34d58e46aee6450981409dfadac5
Taha Saif <tahasaifsurhio454@gmail.com>
2025-01-01 10:03:03 +0500

    uploaded code

--- next-sanity-blog/app/layout.tsx
+++ next-sanity-blog/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="min-h-screen bg-gray-100 text-gray-900">
+      <Analytics />
         {children}
       </body>
     </html>
