f622a11ab8ea5a2ee40c3e0f6dee7aa6009ccd21
Taha Saif <tahasaifsurhio454@gmail.com>
2025-01-26 11:33:57 +0500

    Update layout.tsx

--- next-sanity-blog/app/layout.tsx
+++ next-sanity-blog/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <Head>
+      <meta name="google-adsense-account" content="ca-pub-4686305629312312"/>
       <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
